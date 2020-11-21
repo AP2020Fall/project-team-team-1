a085fb069b3f6d035afd1b2721292637ba56465e
ata <atarahimzadeh.1378@gmail.com>
2020-11-22 02:01:00 +0330

    creat PlayerInfo class.

---
+++ src/main/java/Controller/PlayerController/PlayerInfo.java
@@ -0,0 +1,23 @@
+package Controller.PlayerController;
+
+public class PlayerInfo {
+
+    public static void showBasicInformation(String username){
+
+    }
+    public static void showUserAge(String username) {
+
+    }
+    public static void showUserGamesStatistics(String username , String gameName) {
+
+    }
+    public static void showUserLastPlayed(String username) {
+
+    }
+    public static void show(String username) {
+
+    }
+    public static void showUserFavoritesGame(String username) {
+
+    }
+}
