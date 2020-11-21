6138b7aa144805652f8d514be1a1c7d68d4c8d8e
hessamasna <hessamasna@yahoo.com>
2020-11-21 23:05:20 +0330

    Make PlayerLists Class for adminController

---
+++ src/main/java/Controller/AdminController/PlayerLists.java
@@ -0,0 +1,15 @@
+package Controller.AdminController;
+
+public class PlayerLists {
+
+    public static void showAllUsers() {
+
+    }
+
+    public static void showUsersByUserName(String userName) {
+
+    }
+
+
+
+}
