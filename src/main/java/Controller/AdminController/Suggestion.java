f7acf84db174bead15e22ccca844bedb3e9141fe
hessamasna <hessamasna@yahoo.com>
2020-11-21 23:05:41 +0330

    Make Suggestion Class for adminController

---
+++ src/main/java/Controller/AdminController/Suggestion.java
@@ -0,0 +1,21 @@
+package Controller.AdminController;
+
+public class Suggestion {
+    public static void addSuggestion() {
+
+    }
+
+    public static void showSuggestion() {
+
+    }
+
+    public static void removeSuggestion() {
+
+    }
+
+    private boolean checkSuggestionExistence() {
+        boolean result = false;
+        return result;
+    }
+
+}
