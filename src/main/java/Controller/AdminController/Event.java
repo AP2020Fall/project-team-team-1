d5853656776121cea5c59d599e6c57ff53651a85
hessamasna <hessamasna@yahoo.com>
2020-11-21 23:04:30 +0330

    Make Event Class for adminController

---
+++ src/main/java/Controller/AdminController/Event.java
@@ -0,0 +1,28 @@
+package Controller.AdminController;
+
+public class Event {
+    public static void addEvent() {
+
+    }
+
+    public static void showEvent() {
+
+    }
+
+    public static void editEvent() {
+
+    }
+
+    public static void removeEvent() {
+
+    }
+    public static void eventDateChecker() {
+
+    }
+    private boolean checkEventExistence() {
+        boolean result = false;
+        return result;
+    }
+
+
+}
