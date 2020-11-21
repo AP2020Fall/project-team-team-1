e53dcd6cb6b177f36dc8901c6606c986d5d6a7ab
ata <atarahimzadeh.1378@gmail.com>
2020-11-22 01:31:43 +0330

    creat Line and Lines class.

--- src/main/java/Controller/DotsAndBoxesController/Line.java
+++ src/main/java/Controller/DotsAndBoxesController/Line.java
@@ -1,5 +1,63 @@
 package Controller.DotsAndBoxesController;
 
-public class Line {
+import java.util.ArrayList;
 
+public class Line extends Lines {
+
+    public final String EMPTY = null;
+    public final String HORI_LINE = null ;
+    public final String VERT_LINE = null ;
+    private ArrayList<Box> boxes = new ArrayList<>();
+    private Player owner ;
+    private Dots first ;
+    private Dots second ;
+    private Box box ;
+
+    public Line(int rows, int columns, Dots[][] dots, Dots first, Dots second) {
+        super(rows, columns, dots);
+        this.first = first;
+        this.second = second;
+    }
+
+    public ArrayList<Box> getBoxes() {
+        return boxes;
+    }
+
+    public Player getOwner() {
+        return owner;
+    }
+
+    public Dots getFirst() {
+        return first;
+    }
+
+    public Dots getSecond() {
+        return second;
+    }
+
+    public void setBox(Box box) {
+        this.box = box;
+    }
+
+
+    public boolean hasOwner(){
+        return true;
+    }
+    public void claim(Player player){
+
+    }
+
+    @Override
+    public String toString() {
+        return "Line{" +
+                "boxes=" + boxes +
+                ", owner=" + owner +
+                ", first=" + first +
+                ", second=" + second +
+                ", box=" + box +
+                '}';
+    }
+    public boolean equals (Object object) {
+        return true ;
+    }
 }
--- src/main/java/Controller/DotsAndBoxesController/Lines.java
+++ src/main/java/Controller/DotsAndBoxesController/Lines.java
@@ -1,4 +1,21 @@
 package Controller.DotsAndBoxesController;
 
-public class Lines {
+import java.util.ArrayList;
+
+public class Lines extends GameBoard {
+    private ArrayList<Line> lines = new ArrayList<>();
+
+    public Lines(int rows, int columns, Dots[][] dots) {
+        super(rows, columns, dots);
+    }
+
+    public Line getLine (int row1, int column1, int row2, int column2){
+        Line line = null;
+        return line;
+    }
+    public  int size(){
+        return 0;
+    }
+
+
 }
