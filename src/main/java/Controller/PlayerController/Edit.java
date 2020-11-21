999f7707c9ddb20e0d555fc76463037cc48d954d
ata <atarahimzadeh.1378@gmail.com>
2020-11-22 01:40:36 +0330

    creat Edit class.

--- src/main/java/Controller/PlayerController/Edit.java
+++ src/main/java/Controller/PlayerController/Edit.java
@@ -1,4 +1,24 @@
 package Controller.PlayerController;
 
 public class Edit {
+    public static void editFirstName(){
+
+    }
+
+    public static void editLastName(){
+
+    }
+
+    public static void editPassword(){
+
+    }
+
+    public static void editEmail(){
+
+    }
+
+    public static void editPhoneNumber(){
+
+    }
+
 }
