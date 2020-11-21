df72edf5a4c695cdae474fa2e0ce018a7aa23590
ata <atarahimzadeh.1378@gmail.com>
2020-11-22 01:50:22 +0330

    creat FavoriteGames class.

---
+++ src/main/java/Controller/PlayerController/FavoriteGames.java
@@ -0,0 +1,17 @@
+package Controller.PlayerController;
+
+public class FavoriteGames {
+    public static void addGameToFavoritesGames (){
+
+    }
+    public static void addGameRemoveFavoritesGames (){
+
+    }
+    public static void showFavoritesGames (){
+
+    }
+    private boolean checkFavoriteGameExistence() {
+        boolean result = false;
+        return result;
+    }
+}
