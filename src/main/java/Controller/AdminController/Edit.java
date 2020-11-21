2a52f4f913f6d39f49c9262458a3e01b6dee2d5c
hessamasna <hessamasna@yahoo.com>
2020-11-21 23:04:09 +0330

    Make Edit Class for adminController

---
+++ src/main/java/Controller/AdminController/Edit.java
@@ -0,0 +1,23 @@
+package Controller.AdminController;
+
+public class Edit {
+    public static void editName() {
+
+    }
+
+    public static void editLastName() {
+
+    }
+
+    public static void editPassword() {
+
+    }
+
+    public static void editEmail() {
+
+    }
+
+    public static void editPhoneNumber() {
+
+    }
+}
