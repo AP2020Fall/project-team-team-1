ca79259ddd384f31e6ab747d1c3c3de47d8ac489
ata <atarahimzadeh.1378@gmail.com>
2020-11-22 01:03:48 +0330

    complete Existence class.

--- src/main/java/Controller/CompetencyController/Existence.java
+++ src/main/java/Controller/CompetencyController/Existence.java
@@ -1,4 +1,20 @@
 package Controller.CompetencyController;
 
 public class Existence {
+
+        public static boolean checkUserNameExistence(String userName){
+            boolean result = false;
+            return result;
+        }
+        public static boolean checkEmailExistence(String email){
+            boolean result = false;
+            return result;
+        }
+        public static boolean checkPassword(String username,String password){
+            boolean result = false;
+            return result;
+        }
+
 }
+
+
