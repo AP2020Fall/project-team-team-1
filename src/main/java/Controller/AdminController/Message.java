fc6569b70028b49ff5594030b3142d808cd71a59
hessamasna <hessamasna@yahoo.com>
2020-11-21 23:04:51 +0330

    Make Message Class for adminController

---
+++ src/main/java/Controller/AdminController/Message.java
@@ -0,0 +1,17 @@
+package Controller.AdminController;
+
+public class Message {
+
+    public static Message writeMassage(String text){
+        Message message =null;
+        return message;
+    }
+
+    public static void sendMassage (String username, Message message){
+
+    }
+    public static void showPlayerMassage(String username){
+
+    }
+
+}
