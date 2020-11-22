d689585b46a4123d81a3a168df8ce844d21045c1
yasminkadkhodaei <59218135+yasminkadkhodaei@users.noreply.github.com>
2020-11-22 12:50:54 +0330

    edit class done

---
+++ src/main/java/Controller/PlayerController/Edit.java
@@ -0,0 +1,26 @@
+package Controller.PlayerController;
+
+public class Edit {
+
+    public static void editFirstName(){
+
+    }
+
+    public static void editLastName(){
+
+    }
+
+    public static void editPassword(){
+
+    }
+
+    public static void editEmail(){
+
+    }
+
+    public static void editPhoneNumber(){
+
+    }
+
+
+}
