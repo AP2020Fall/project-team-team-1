d4270d3292730acd2273d89ea81f6ae8ccdda473
hessamasna <hessamasna@yahoo.com>
2020-11-22 14:47:50 +0330

    Complete Existence methods

--- src/main/java/Controller/CompetencyController/Existence.java
+++ src/main/java/Controller/CompetencyController/Existence.java
@@ -10,19 +10,39 @@ public class Existence {
     public static boolean CheckUserNameExistence(String userName) {
         boolean result = false;
         for (User user : Player.users) {
-            if (user.getUserName().equals(userName))
+            if (user.getUserName().equals(userName)){
                 result = true;
+                break;
+            }
         }
         return result;
     }
 
     public static boolean CheckEmailExistence(String email) {
         boolean result = false;
+        for (User user : Player.users) {
+            if (user.getEmail().equals(email)){
+                result = true;
+                break;
+            }
+        }
         return result;
     }
 
     public static boolean CheckPassword(String username, String password) {
         boolean result = false;
+        User userByUsername = null;
+        for (User user : Player.users) {
+            if (user.getUserName().equals(username)){
+                userByUsername = user;
+                break;
+            }
+        }
+        if (userByUsername != null){
+            if (userByUsername.getPassword().equals(password)){
+                result = true;
+            }
+        }
         return result;
     }
         public static boolean AdminExistence(){
