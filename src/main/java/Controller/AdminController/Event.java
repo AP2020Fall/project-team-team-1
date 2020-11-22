01e5f6495eb5bb8bb109144eb504f94e074658d4
hessamasna <hessamasna@yahoo.com>
2020-11-23 02:01:22 +0330

    work on Event

--- src/main/java/Controller/AdminController/Event.java
+++ src/main/java/Controller/AdminController/Event.java
@@ -1,27 +1,64 @@
 package Controller.AdminController;
 
+import Controller.CompetencyController.Existence;
+
+import java.util.ArrayList;
+
 public class Event {
-    public static void addEvent() {
+    public static void addEvent(String input) {
+        String[] inputSpilt = input.split("\\s");
+        //Model.PlatoModel.Event.addNewEvent(new Model.PlatoModel.Event());
 
     }
 
     public static void showEvent() {
+        eventDateChecker();
+        for (Model.PlatoModel.Event event : Model.PlatoModel.Event.events) {
+            System.out.println("EventId: "+event.getEventID()+"Game name: "+event.getGameName()+"Start date: "+event.getStartDate()+"End date: "+event.getEndDate()+"Score: "+event.getScore());
+        }
+    }
+
+    public static void editEvent(String eventID,String field, String input) {
+        eventDateChecker();
+        if (!Existence.checkEventExistence(Integer.parseInt(eventID))){
+            System.out.println("There is no Event with this it");
+        }else {
+
+        }
 
     }
 
-    public static void editEvent() {
-
-    }
-
-    public static void removeEvent() {
-
+    public static void removeEvent(String eventID) {
+        if (Existence.checkEventExistence(Integer.parseInt(eventID))){
+            Model.PlatoModel.Event.events.remove(eventFinderByEventID(eventID));
+        }
     }
     public static void eventDateChecker() {
+        ArrayList<Model.PlatoModel.Event> listForDelete = new ArrayList<>();
+        for (Model.PlatoModel.Event event : Model.PlatoModel.Event.getEvents()) {
+            if (event.getStartDate().after(event.getEndDate())){
+                listForDelete.add(event);
+            }
+        }
+
+        if (listForDelete.size() != 0){
+            for (Model.PlatoModel.Event eventForDelete : listForDelete) {
+                removeEvent(String.valueOf(eventForDelete.getEventID()));
+            }
+        }
+
 
     }
-    private boolean checkEventExistence() {
-        boolean result = false;
-        return result;
+
+    private static Model.PlatoModel.Event eventFinderByEventID(String eventID){
+        Model.PlatoModel.Event resultEvent = null;
+        for (Model.PlatoModel.Event event: Model.PlatoModel.Event.getEvents()) {
+            if (event.getEventID() == Integer.parseInt(eventID)){
+                resultEvent = event;
+                break;
+            }
+        }
+        return resultEvent;
     }
 
 
