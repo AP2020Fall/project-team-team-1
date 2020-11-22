df7bbf0a8cb874ac618c67fcbc0c13824fa06ae9
yasminkadkhodaei <59218135+yasminkadkhodaei@users.noreply.github.com>
2020-11-22 12:58:03 +0330

    Message class in model done

---
+++ src/main/java/Model/PlatoModel/Message.java
@@ -0,0 +1,29 @@
+package Model.PlatoModel;
+
+import java.util.ArrayList;
+
+public class Message {
+    private static ArrayList<Message> messages = new ArrayList<>();
+    private int massageID=0;
+    private String text;
+    private Player receiver;
+
+    public Message(String text, Player receiver) {
+        this.text = text;
+        this.receiver = receiver;
+        this.massageID=massageID();
+    }
+    private int massageID(){
+        massageID++;
+        return massageID;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "massageID=" + massageID +
+                ", text='" + text + '\'' +
+                ", receiver=" + receiver +
+                '}';
+    }
+}
