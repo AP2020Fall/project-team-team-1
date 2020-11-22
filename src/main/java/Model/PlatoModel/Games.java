a9520fd1d21b38fb6675407a0f0d9d538bae2f38
yasminkadkhodaei <59218135+yasminkadkhodaei@users.noreply.github.com>
2020-11-22 13:01:18 +0330

    Games class in model done

---
+++ src/main/java/Model/PlatoModel/Games.java
@@ -0,0 +1,10 @@
+package Model.PlatoModel;
+
+import java.util.ArrayList;
+
+public class Games {
+    private static ArrayList<Games> games = new ArrayList<Games>();
+    private int gameID;
+    private String gameName;
+
+}
