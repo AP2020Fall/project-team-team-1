69d2dd3c049d804027ed7f1c755ddbc19331d74b
Amirzgh1 <Amirgh5665@gmail.com>
2020-11-22 13:27:17 +0330

    register controller

--- src/main/java/Controller/RegisterController/LogIn.java
+++ src/main/java/Controller/RegisterController/LogIn.java
@@ -1,4 +1,10 @@
 package Controller.RegisterController;
 
 public class LogIn {
+    private void loginAsPlayer(){
+
+    }
+    private void loginAsAdmin(){
+
+    }
 }
---
+++ src/main/java/Controller/RegisterController/SignUp.java
@@ -0,0 +1,11 @@
+package Controller.RegisterController;
+
+
+public class SignUp {
+    private void addAdmin(){
+
+    }
+    private void addPlayer(){
+
+    }
+}
