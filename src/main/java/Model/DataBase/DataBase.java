54e47345e31bb10f548cbc4d184f9881343d12d1
yasminkadkhodaei <59218135+yasminkadkhodaei@users.noreply.github.com>
2020-11-22 13:08:21 +0330

    Database class in model done

--- src/main/java/Model/DataBase/DataBase.java
+++ src/main/java/Model/DataBase/DataBase.java
@@ -1,4 +1,23 @@
 package Model.DataBase;
 
+import java.util.ArrayList;
+
 public class DataBase {
+    //private static Gson Gson = new Gson();
+
+    public static void save(Object object, boolean New) {
+
+    }
+
+    public static void saveWithArrayList(ArrayList arrayList, boolean New) {
+
+    }
+
+    public static void remove(Object object) {
+
+    }
+
+    public static void load(Object object, boolean New) {
+
+    }
 }
