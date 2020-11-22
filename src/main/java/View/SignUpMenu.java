1e2635ed3e7eac7a05fc1453c9bb080e1b6e61b3
Amirzgh1 <Amirgh5665@gmail.com>
2020-11-22 13:14:50 +0330

    comment

--- src/main/java/View/SignUpMenu.java
+++ src/main/java/View/SignUpMenu.java
@@ -11,14 +11,14 @@ public class SignUpMenu extends Menu {
         HashMap<Integer,Menu> submenus = new HashMap<>();
 
     }
-    private Menu registerAsAdmin(){
-        return new Menu("register Admin",this) {
-            @Override
-            public void show() {
-                System.out.println(this.getName());
-            }
-        }
-    }
+//    private Menu registerAsAdmin(){
+//        return new Menu("register Admin",this) {
+//            @Override
+//            public void show() {
+//                System.out.println(this.getName());
+//            }
+//        }
+//    }
     private void getManagerInformation(ArrayList<String> managerInfo){
         System.out.println("Hello our Dear Admin \n Please notice that if there is already an admin you can not register as Admin !");
         System.out.println("Please Enter Your Firstname :");
