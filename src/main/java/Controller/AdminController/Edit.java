91301b2c3632a7d47259c519255d8492521082ed
hessamasna <hessamasna@yahoo.com>
2020-11-22 18:08:02 +0330

    Edit Field founder Completed

--- src/main/java/Controller/AdminController/Edit.java
+++ src/main/java/Controller/AdminController/Edit.java
@@ -1,23 +1,41 @@
 package Controller.AdminController;
 
 public class Edit {
-    public static void editName() {
+    public static void editField(String field, String input) {
+        if (field.trim().equalsIgnoreCase("name")) {
+            editName(input);
+        } else if (field.trim().equalsIgnoreCase("lastname")) {
+            editLastName(input);
+        } else if (field.trim().equalsIgnoreCase("email")) {
+            editEmail(input);
+        } else if (field.trim().equalsIgnoreCase("phonenumber")) {
+            editPhoneNumber(input);
+        } else if (field.trim().equalsIgnoreCase("username")) {
+            editUsername(input);
+        } else
+            System.out.println("Field for edit is InValid !");
+    }
+
+    protected static void editName(String input) {
 
     }
 
-    public static void editLastName() {
+    protected static void editLastName(String input) {
 
     }
 
-    public static void editPassword() {
+    protected static void editEmail(String input) {
+
+    }
+    protected static void editUsername(String input) {
 
     }
 
-    public static void editEmail() {
+    protected static void editPhoneNumber(String input) {
 
     }
 
-    public static void editPhoneNumber() {
+    public static void editPassword(String oldPassword, String newPassword) {
 
     }
 }
