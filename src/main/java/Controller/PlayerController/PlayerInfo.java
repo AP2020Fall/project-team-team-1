64be301cd36765448821731373cf45414008576c
hessamasna <hessamasna@yahoo.com>
2020-11-24 22:09:34 +0330

    Make some Changes in Player in Model Completes accept and Decline Work on Friend

--- src/main/java/Controller/PlayerController/PlayerInfo.java
+++ src/main/java/Controller/PlayerController/PlayerInfo.java
@@ -1,12 +1,25 @@
 package Controller.PlayerController;
 
+import Model.PlatoModel.Player;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 public class PlayerInfo {
 
     public static void showBasicInformation(String username){
 
     }
     public static void showUserAge(String username) {
-
+        Player player = FindPlayerByInfo.findByUserName(username);
+        LocalDate registerDate = player.getRegisterDate();
+        LocalDate now = LocalDate.now();
+//        LocalDate start = LocalDate.parse("2020-10-02");
+//        LocalDate now = LocalDate.now();
+//        System.out.println(start);
+//        System.out.println(now);
+//        Long between = ChronoUnit.DAYS.between(start,now);
+//        System.out.println(between);
     }
     public static void showUserGamesStatistics(String username , String gameName) {
 
