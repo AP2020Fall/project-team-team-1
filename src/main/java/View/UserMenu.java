5494dd6f70b557b4efcf4329bedf1fc3df43167e
Amirzgh1 <Amirgh5665@gmail.com>
2020-11-24 11:14:09 +0330

    AdminMainMenu 40%

--- src/main/java/View/UserMenu.java
+++ src/main/java/View/UserMenu.java
@@ -1,16 +1,24 @@
 package View;
 
 import java.util.HashMap;
+import Controller.GeneralController.UserController;
 
 public class UserMenu extends Menu {
     String username;
-    public UserMenu(String username) {
-        super("User Menu", null);
+    public UserMenu(String username,Menu parentMenu) {
+        super("User Menu", parentMenu);
         this.username=username;
         HashMap<Integer,Menu> subMenus = new HashMap<>();
+        subMenus.put(1,ShowUserInfo());
+
     }
     private Menu ShowUserInfo(){
      return new Menu("View User Information", this) {
+         @Override
+         public void show() {
+             System.out.println("hey "+username+" here is your profile.");
+             userController.showUserInfo(username);
+         }
 
      };
     }
