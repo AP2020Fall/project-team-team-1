d8e9c89a9337e08e39c35921fad52f881b7b6040
ata <atarahimzadeh.1378@gmail.com>
2020-11-24 23:39:13 +0330

    nothing.

--- src/main/java/Controller/PlayerController/JoinEvent.java
+++ src/main/java/Controller/PlayerController/JoinEvent.java
@@ -2,18 +2,14 @@ package Controller.PlayerController;
 
 public class JoinEvent {
 
-    private boolean checkEventExistence() {
-        boolean result = false;
-        return result;
-    }
     private static void playEvent(){
 
     }
-    private void giveWinnerScore(String username){
-
-    }
-    private void giveWinnerCoin(String username){
-
-    }
+//    private void giveWinnerScore(String username){
+//
+//    }
+//    private void giveWinnerCoin(String username){
+//
+//    }
 
 }
