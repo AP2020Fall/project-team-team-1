b0489222651a61174ddc9106b7297c3a11fde457
ata <atarahimzadeh.1378@gmail.com>
2020-11-28 04:16:49 +0330

    LogInTest Exception.

--- src/test/java/RegisterController/SignUpTest.java
+++ src/test/java/RegisterController/SignUpTest.java
@@ -1,5 +1,8 @@
 package RegisterController;
 
+import Controller.Exception.ExistAdminException;
+import Controller.Exception.ExistEmailException;
+import Controller.Exception.ExistUserNameException;
 import Controller.PlayerController.FindPlayerByInfo;
 import Controller.RegisterController.SignUp;
 import Model.PlatoModel.Admin;
@@ -9,21 +12,21 @@ import org.junit.Test;
 
 public class SignUpTest {
 
-//    @Test
-//    public void testAddAdmin() {
-//        SignUp signUp = new SignUp();
-//        signUp.addAdmin("yasmin kadkhodaei yamsiin 007Password yasmiinkad@gmail.com 09129749527");
-//        Assert.assertFalse(Admin.getAdmins().isEmpty());
-//        signUp.addAdmin("hesam asnaashari hesamasna 008Password hesamasna@yahoo.com 09121112233");
-//        Assert.assertEquals(Admin.getAdmins().size(), 1);
-//        Assert.assertEquals(FindPlayerByInfo.findByUserName("yamsiin").getUserName(), "yamsiin");
-////        Assert.assertEquals(, "007Password");
-////        Assert.assertEquals(, "yasmiinkad@gmail.com");
-//
-//    }
+    @Test
+    public void testAddAdmin() throws ExistAdminException, ExistEmailException, ExistUserNameException {
+        SignUp signUp = new SignUp();
+        signUp.addAdmin("yasmin kadkhodaei yamsiin 007Password yasmiinkad@gmail.com 09129749527");
+        Assert.assertFalse(Admin.getAdmins().isEmpty());
+        signUp.addAdmin("hesam asnaashari hesamasna 008Password hesamasna@yahoo.com 09121112233");
+        Assert.assertEquals(Admin.getAdmins().size(), 1);
+        Assert.assertEquals(FindPlayerByInfo.findByUserName("yamsiin").getUserName(), "yamsiin");
+//        Assert.assertEquals(, "007Password");
+//        Assert.assertEquals(, "yasmiinkad@gmail.com");
+
+    }
 
     @Test
-    public void testAddPlayer() {
+    public void testAddPlayer() throws ExistEmailException, ExistUserNameException {
         SignUp signUp = new SignUp();
         signUp.addPlayer("ata rhz atarhz 008Password atarhz@gmail.com 09129709521");
         Assert.assertTrue(Player.players.contains(FindPlayerByInfo.findByUserName("atarhz")));
