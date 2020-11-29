bed367ec5f2c973d1d0f8b854f881bb1201b3c4e
hessamasna <hessamasna@yahoo.com>
2020-11-29 17:44:13 +0330

    Work on Error Handling

--- src/main/java/Controller/AdminController/Edit.java
+++ src/main/java/Controller/AdminController/Edit.java
@@ -6,7 +6,7 @@ import Controller.Exception.*;
 import Model.PlatoModel.Admin;
 
 public class Edit {
-    public static void editField(String field, String input) throws InvalidNameException, InvalidEmailException, InvalidPhoneNumberException, ExistEmailException {
+    public static void editField(String field, String input) throws InvalidNameException, InvalidEmailException, InvalidPhoneNumberException, ExistEmailException, InvalidFieldException {
         if (field.trim().equalsIgnoreCase("name")) {
             editName(input);
         } else if (field.trim().equalsIgnoreCase("lastname")) {
@@ -20,27 +20,22 @@ public class Edit {
 //            editUsername(input);
 //        }
         else
-            System.out.println("Field for edit is InValid !");
+            throw new InvalidFieldException("Field for Edit is Invalid");
     }
 
     protected static void editName(String input) throws InvalidNameException {
-        boolean pass = false;
-        pass = Validation.nameOrLastNameIsValid(input);
-        if (!pass)
-            System.out.println("Format is InValid !");
+        Validation.nameOrLastNameIsValid(input);
 
-        if (pass)
-            Admin.getAdmins().get(0).setName(input);
+        Admin.getAdmins().get(0).setName(input);
     }
 
     protected static void editLastName(String input) throws InvalidNameException {
-        boolean pass = false;
-        pass = Validation.nameOrLastNameIsValid(input);
-        if (!pass)
-            System.out.println("Format is InValid !");
+        Validation.nameOrLastNameIsValid(input);
 
-        if (pass)
-            Admin.getAdmins().get(0).setLastName(input);
+        System.out.println("Format is InValid !");
+
+
+        Admin.getAdmins().get(0).setLastName(input);
     }
 
     protected static void editEmail(String input) throws InvalidEmailException, ExistEmailException {
@@ -79,31 +74,26 @@ public class Edit {
     protected static void editPhoneNumber(String input) throws InvalidPhoneNumberException {
 
         Validation.phoneNumberIsValid(input);
+
         Admin.getAdmins().get(0).setPhoneNum(input);
 
     }
 
-    public static void editPassword(String oldPassword, String newPassword) throws InvalidPasswordException {
+    public static void editPassword(String oldPassword, String newPassword) throws InvalidPasswordException, WrongPasswordException {
         boolean pass = false;
-        pass = Existence.checkPassword(Admin.getAdmins().get(0).getUserName(), oldPassword);
-        if (!pass)
-            System.out.println("Password is InValid !");
 
-        if (pass) {
-            if (oldPassword.equals(newPassword)) {
-                System.out.println("Old password and new password are same! please enter new password");
-                pass = false;
-            }
+        if (!Existence.checkPassword(Admin.getAdmins().get(0).getUserName(), oldPassword))
+            throw new WrongPasswordException();
 
-            if (pass) {
-                pass = Validation.passwordIsValid(newPassword);
-                if (!pass)
-                    System.out.println("Make Stranger Password (read Hits for make good Password)");
+        if (oldPassword.equals(newPassword))
+            throw new InvalidPasswordException("Old password and new password are same! please enter new password");
 
-                if (pass)
-                    Admin.getAdmins().get(0).setPassword(newPassword);
-            }
 
-        }
+        Validation.passwordIsValid(newPassword);
+
+
+        Admin.getAdmins().get(0).setPassword(newPassword);
+
+
     }
 }
