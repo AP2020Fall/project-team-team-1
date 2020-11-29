c61261da6ee9704fabf62080ccbf786c1f055ae1
Amirzgh1 <Amirgh5665@gmail.com>
2020-11-29 13:54:04 +0330

    Admin logout is complete

--- src/main/java/View/UserMenuForAdmin.java
+++ src/main/java/View/UserMenuForAdmin.java
@@ -5,11 +5,26 @@ import java.util.HashMap;
 public class UserMenuForAdmin extends Menu {
     String username;
     public UserMenuForAdmin(String username, Menu parentMenu) {
-        super("User Menu", parentMenu);
+        super("Profile", parentMenu);
         this.username=username;
         HashMap<Integer,Menu> subMenus = new HashMap<>();
         subMenus.put(1,new ShowAdminInfo(this.username,this));
+        subMenus.put(2,logout());
         this.setSubmenus(subMenus);
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    private Menu logout(){
+        return new Menu("logout",this) {
+            @Override
+            public void execute() {
+            setUsername(null);
+            this.parentMenu.parentMenu.parentMenu.parentMenu.parentMenu.run();
+            }
+        };
     }
 
     @Override
