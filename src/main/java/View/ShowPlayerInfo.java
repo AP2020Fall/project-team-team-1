c2c834afa4f21d439bfc984eb588a85af3d89114
Amirzgh1 <Amirgh5665@gmail.com>
2020-11-29 23:03:31 +0330

    ShowPlayerInfo is almost Completed just exception remains

--- src/main/java/View/ShowPlayerInfo.java
+++ src/main/java/View/ShowPlayerInfo.java
@@ -1,5 +1,8 @@
 package View;
 
+import Controller.Exception.*;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ShowPlayerInfo extends Menu {
@@ -8,7 +11,100 @@ public class ShowPlayerInfo extends Menu {
         super("User info", parentMenu);
         this.username=username;
         HashMap<Integer,Menu> submenus = new HashMap<>();
-
+        submenus.put(1,changePassword());
+        submenus.put(2,changeAfield());
         this.setSubmenus(submenus);
+    }
+    private Menu changePassword(){
+        return new Menu("change Password",this) {
+            @Override
+            public void execute() {
+                ArrayList<String> info = new ArrayList<>();
+                info.add(username);
+                getPasswords(info);
+                try {
+                    playerGeneralController.editPassword(arrayListToString(info));
+                    System.out.println("Change password Successfully");
+                    this.parentMenu.run();
+                } catch (InvalidPasswordException e) {
+                    System.out.println(e.getMessage());
+                    this.run();
+                } catch (WrongPasswordException e) {
+                    System.out.println(e.getMessage());
+                } catch (ExistPlayerException e) {
+                    System.out.println(e.getPlayerName() + e.getMessage());
+                }
+            }
+        };
+
+    }
+    private Menu changeAfield(){
+        return new Menu("Change name | Lastname | Email | phoneNum", this) {
+            @Override
+            public void execute() {
+                ArrayList<String> info = new ArrayList<>();
+                getChangeINfo(info);
+                //todo exceptions not completed!!!! kad dont touch it!!!!!!!!
+                try {
+                    playerGeneralController.editField(arrayListToString(info));
+                    System.out.println(info.get(0)+" changed successfully.");
+                } catch (InvalidNameException e) {
+                    e.printStackTrace();
+                } catch (InvalidEmailException e) {
+                    e.printStackTrace();
+                } catch (InvalidPhoneNumberException e) {
+                    e.printStackTrace();
+                } catch (ExistEmailException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+    }
+    private void getChangeINfo(ArrayList<String> changeInfo){
+        System.out.println("Enter The field you want to change : ");
+        String field = scanner.nextLine();
+        changeInfo.add(field);
+        System.out.println("Enter "+ field +" new Value : ");
+        String newValue = scanner.nextLine();
+        changeInfo.add(newValue);
+    }
+    private void getPasswords(ArrayList<String> passwords){
+        System.out.println("Please Enter Your Previous Password");
+        String oldPass = scanner.nextLine();
+        passwords.add(oldPass);
+        System.out.println("Please Enter Your New Password");
+        String newPass = scanner.nextLine();
+        passwords.add(newPass);
+    }
+
+    @Override
+    public void execute() {
+        playerGeneralController.showBasicInformation(username);
+        Menu nextMenu = null;
+        String num = scanner.nextLine();
+        if ((!num.matches("\\d+")) || Integer.parseInt(num) > submenus.size() + 1){
+            this.run();
+        }else {
+            int chosenMenu = Integer.parseInt(num);
+            if (chosenMenu==submenus.size()+1){
+                if (this.parentMenu==null){
+                    System.exit(1);
+                }else {
+                    nextMenu=this.parentMenu;
+                    nextMenu.run();
+                }
+            } else {
+                nextMenu = submenus.get(chosenMenu);
+                nextMenu.run();
+            }
+        }
+    }
+
+    public static String arrayListToString(ArrayList<String> arrayList) {
+        String output = "";
+        for (String string : arrayList) {
+            output += string + " ";
+        }
+        return output;
     }
 }
