aed778b32545bb8a347679bdbb72ffe570d6c86d
hessamasna <hessamasna@yahoo.com>
2020-12-01 01:48:51 +0330

    Update Gson in Pom.xml Completed save method in DataBase in Model

--- src/main/java/Model/DataBase/DataBase.java
+++ src/main/java/Model/DataBase/DataBase.java
@@ -1,12 +1,30 @@
 package Model.DataBase;
 
+import com.google.gson.Gson;
+
+import java.io.*;
 import java.util.ArrayList;
 
 public class DataBase {
-    //private static Gson Gson = new Gson();
+    private static com.google.gson.Gson gson = new Gson();
+    private static ArrayList<File> filesForLoad = new ArrayList<>();
 
-    public static void save(Object object, boolean New) {
+    public static void save(ArrayList object, File file) throws IOException {
 
+        if (file.exists())
+            file.delete();
+
+        file.createNewFile();
+
+        String outForData = new Gson().toJson(object);
+
+//        FileWriter fWrite = null;
+//        BufferedWriter bWrite = null;
+
+        FileWriter fWrite = new FileWriter(file, true); // true for appending content to the existing file
+        BufferedWriter bWrite = new BufferedWriter(fWrite);
+        bWrite.write(outForData);
+        bWrite.close();
     }
 
     public static void saveWithArrayList(ArrayList arrayList, boolean New) {
