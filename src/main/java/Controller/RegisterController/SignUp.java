bf4df4a7a1d6d96d70c68bd02d81c84c2a082954
ata <atarahimzadeh.1378@gmail.com>
2020-12-03 13:25:21 +0330

    last name exception for admin.

--- src/main/java/Controller/RegisterController/SignUp.java
+++ src/main/java/Controller/RegisterController/SignUp.java
@@ -8,7 +8,7 @@ import Model.PlatoModel.Player;
 import java.util.Random;
 
 public class SignUp {
-    public void addAdmin(String adminInfo) throws ExistAdminException, ExistEmailException, ExistUserNameException, EmptyExceptionForName {
+    public void addAdmin(String adminInfo) throws ExistAdminException, ExistEmailException, ExistUserNameException, EmptyExceptionForName, EmptyExceptionForLastName {
 
         String[] adminInfoSplit = adminInfo.split("\\s");
 
@@ -30,6 +30,9 @@ public class SignUp {
         }
         if ((adminInfoSplit[0].isEmpty())) {
             throw new EmptyExceptionForName("Name field can not be empty");
+        }
+        if (adminInfoSplit[1].isEmpty()){
+            throw new EmptyExceptionForLastName("Last name field can not be empty");
         }
 
         Admin.AddNewAdmin( new Admin(adminInfoSplit[0], adminInfoSplit[1], 1000 , adminInfoSplit[2], adminInfoSplit [4], adminInfoSplit[3], adminInfoSplit[5]));
