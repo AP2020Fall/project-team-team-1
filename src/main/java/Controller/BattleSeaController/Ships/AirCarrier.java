c161cd6859c3611f23cf4e4fd86822803cc00345
hessamasna <hessamasna@yahoo.com>
2020-12-04 01:02:28 +0330

    Add AirCarrier

---
+++ src/main/java/Controller/BattleSeaController/Ships/AirCarrier.java
@@ -0,0 +1,9 @@
+package Controller.BattleSeaController.Ships;
+
+import Controller.BattleSeaController.Coordinate.Coordinate;
+
+public class AirCarrier extends Ship {
+    public AirCarrier() {
+        super("AirCarrier", 5, false, null);
+    }
+}
