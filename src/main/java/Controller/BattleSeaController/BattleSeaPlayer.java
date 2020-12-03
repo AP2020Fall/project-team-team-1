5f048997efce72f30fc2406eb8342c45aeaa146b
hessamasna <hessamasna@yahoo.com>
2020-12-04 01:06:05 +0330

    edit and fix BattleSeaPlayer

--- src/main/java/Controller/BattleSeaController/BattleSeaPlayer.java
+++ src/main/java/Controller/BattleSeaController/BattleSeaPlayer.java
@@ -5,20 +5,32 @@ import Controller.BattleSeaController.Ships.*;
 
 import java.util.ArrayList;
 
-public class BattleSeaPlayer{
+public class BattleSeaPlayer {
+    public static ArrayList<BattleSeaPlayer> battleSeaPlayers = new ArrayList<>();
+    private ArrayList<String> playerBooms;
+    private ArrayList<String> correctPlayerBooms;
+    private ArrayList<String> inCorrectPlayerBooms;
     private String player;
     private int score;
-    private ArrayList<Ship> playerShip ;
+    private ArrayList<Ship> playerShip;
+    private ArrayList<Ship> boomedPlayerShip;
+    private ArrayList<Ship> unBoomedPlayerShip;
 
     public BattleSeaPlayer(String player) {
         this.player = player;
-        this.score=0;
+        this.score = 0;
         playerShip = new ArrayList<Ship>();
         playerShip.add(new Battleship());
-        playerShip.add(new Carrier());
+        playerShip.add(new AirCarrier());
         playerShip.add(new Cruiser());
         playerShip.add(new Destroyer());
         playerShip.add(new Submarine());
+        this.playerBooms = new ArrayList<>();
+        this.correctPlayerBooms = new ArrayList<>();
+        this.inCorrectPlayerBooms = new ArrayList<>();
+        this.unBoomedPlayerShip = new ArrayList<>();
+        unBoomedPlayerShip.addAll(playerShip);
+        this.boomedPlayerShip = new ArrayList<>();
 
     }
 
@@ -42,10 +54,37 @@ public class BattleSeaPlayer{
         this.playerShip = playerShip;
     }
 
+    public ArrayList<String> getPlayerBooms() {
+        return playerBooms;
+    }
+
+    public ArrayList<String> getCorrectPlayerBooms() {
+        return correctPlayerBooms;
+    }
+
+    public ArrayList<String> getInCorrectPlayerBooms() {
+        return inCorrectPlayerBooms;
+    }
+
+    public ArrayList<Ship> getBoomedPlayerShip() {
+        return boomedPlayerShip;
+    }
+
+    public ArrayList<Ship> getUnBoomedPlayerShip() {
+        return unBoomedPlayerShip;
+    }
+    public static void addNewBattleSeaPlayer(BattleSeaPlayer player){
+        battleSeaPlayers.add(player);
+    }
+
+
     @Override
     public String toString() {
         return "BattleSeaPlayer{" +
-                "player='" + player + '\'' +
+                "playerBooms=" + playerBooms +
+                ", correctPlayerBooms=" + correctPlayerBooms +
+                ", inCorrectPlayerBooms=" + inCorrectPlayerBooms +
+                ", player='" + player + '\'' +
                 ", score=" + score +
                 ", playerShip=" + playerShip +
                 '}';
