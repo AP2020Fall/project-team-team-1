fd700e0cefe66d40ff2e67fd22f318f43101df60
ata <atarahimzadeh.1378@gmail.com>
2020-12-05 23:28:29 +0330

    PlayerListsTest done.

---
+++ src/test/java/Controller/AdminController/PlayerListsTest.java
@@ -0,0 +1,38 @@
+package Controller.AdminController;
+
+import Controller.Exception.ExistPlayerException;
+import Controller.PlayerController.FindPlayerByInfo;
+import Controller.PlayerController.PlayerInfo;
+import Model.PlatoModel.Admin;
+import Model.PlatoModel.Player;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerListsTest {
+
+    @Test
+    void showAllUsers() throws ExistPlayerException {
+        Player player = new Player("ata","rhz",1,"atarhz","112233","ata@gmail.com","09121112222");
+        Player.AddNewPlayer(player);
+        PlayerLists.showAllUsers();
+        assertEquals(Player.getPlayers().size(),1);
+    }
+
+    @Test
+    void showUsersByUserName() throws ExistPlayerException {
+        Player player = new Player("ata","rhz",1,"atarhz","112233","ata@gmail.com","09121112222");
+        Player.AddNewPlayer(player);
+        PlayerLists.showUsersByUserName("atarhz");
+        assertEquals(FindPlayerByInfo.findByUserName("atarhz").getUserName(),"atarhz");
+    }
+
+    @Test
+    void showAdminInfo() {
+        Admin admin = new Admin("ata","rhz",1,"atarhz","112233","ata@gmail.com","09121112222");
+        Admin.AddNewAdmin(admin);
+        assertEquals(Admin.getAdmins().size(),1);
+        System.out.println(Admin.getAdmins());
+
+    }
+}
