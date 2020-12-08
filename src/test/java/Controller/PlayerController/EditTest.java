187152b0dd2522ed0fb0f3e1bb13d3b3630430b7
yasminkadkhodaei <59218135+yasminkadkhodaei@users.noreply.github.com>
2020-12-08 22:36:25 +0330

    EditTest updates

--- src/test/java/Controller/PlayerController/EditTest.java
+++ src/test/java/Controller/PlayerController/EditTest.java
@@ -4,24 +4,31 @@ import Controller.Exception.*;
 import Model.PlatoModel.Admin;
 import Model.PlatoModel.Player;
 import junit.framework.TestCase;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
 
-public class EditTest extends TestCase {
+public class EditTest{
+    @BeforeEach
+    public void fundamentals(){
+        Player.AddNewPlayer(new Player("ata","rhz",1,"atarhz","11223344","ataarahimzadeh@gmail.com","09365909061"));
 
+    }
+
+    @Test
     public void testEditField() throws InvalidNameException, InvalidEmailException, InvalidPhoneNumberException, ExistEmailException, InvalidFieldException {
-        Player player = new Player("ata","rhz",1,"atarhz","11223344","ataarahimzadeh@gmail.com","09365909061");
-        Player.AddNewPlayer(player);
 
         Controller.PlayerController.Edit.editField("atarhz","name","amir");
-        assertEquals(player.getName(),"amir");
+        assertEquals(FindPlayerByInfo.findByUserName("atarhz").getName(),"amir");
 
-        Controller.PlayerController.Edit.editField("atarhz","Lastname","Zgh");
-        assertEquals(player.getLastName(),"Zgh");
+        Controller.PlayerController.Edit.editField("atarhz","lastName","Zgh");
+        assertEquals(FindPlayerByInfo.findByUserName("atarhz").getLastName(),"Zgh");
 
         Controller.PlayerController.Edit.editField("atarhz","email","amirZgh1@yahoo.com");
-        assertEquals(player.getEmail(),"amirZgh1@yahoo.com");
+        assertEquals(FindPlayerByInfo.findByUserName("atarhz").getEmail(),"amirZgh1@yahoo.com");
 
         Controller.PlayerController.Edit.editField("atarhz","phonenumber","09122222222");
-        assertEquals(player.getPhoneNum(),"09122222222");
+        assertEquals(FindPlayerByInfo.findByUserName("atarhz").getPhoneNum(),"09122222222");
 
 //        Controller.PlayerController.Edit.editField("atarhz","username","amirrezazgh1");
 //        assertEquals(player.getUserName(),"amirrezazgh1");
@@ -33,39 +40,34 @@ public class EditTest extends TestCase {
 //        Edit.editUsername(player,"hesam");
 //        assertEquals(player.getUserName(),"hesam");
 //    }
-
+    @Test
     public void testEditFirstName() throws InvalidNameException {
-        Player player = new Player("ata","rhz",1,"atarhz","11223344","ataarahimzadeh@gmail.com","09365909061");
-        Player.AddNewPlayer(player);
-        Edit.editFirstName(player,"hesam");
-        assertEquals(player.getName(),"hesam");
-    }
 
+        Edit.editFirstName(FindPlayerByInfo.findByUserName("atarhz"),"hesam");
+        assertEquals(FindPlayerByInfo.findByUserName("atarhz").getName(),"hesam");
+    }
+    @Test
     public void testEditLastName() throws InvalidNameException {
-        Player player = new Player("ata","rhz",1,"atarhz","11223344","ataarahimzadeh@gmail.com","09365909061");
-        Player.AddNewPlayer(player);
-        Edit.editLastName(player,"asna");
-        assertEquals(player.getLastName(),"asna");
-    }
 
+        Edit.editLastName(FindPlayerByInfo.findByUserName("atarhz"),"asna");
+        assertEquals(FindPlayerByInfo.findByUserName("atarhz").getLastName(),"asna");
+    }
+    @Test
     public void testEditPassword() throws InvalidPasswordException, WrongPasswordException, SamePasswordException {
-        Player player = new Player("ata","rhz",1,"atarhz","11223344","ataarahimzadeh@gmail.com","09365909061");
-        Player.AddNewPlayer(player);
+
         Edit.editPassword("atarhz","11223344","hesamKhare22");
-        assertEquals(player.getPassword(),"hesamKhare22");
+        assertEquals(FindPlayerByInfo.findByUserName("atarhz").getPassword(),"hesamKhare22");
     }
-
+    @Test
     public void testEditEmail() throws InvalidEmailException, ExistEmailException {
-        Player player = new Player("ata","rhz",1,"atarhz","11223344","ataarahimzadeh@gmail.com","09365909061");
-        Player.AddNewPlayer(player);
-        Edit.editEmail(player,"hesamasna@yahoo.com");
-        assertEquals(player.getEmail(),"hesamasna@yahoo.com");
-    }
 
+        Edit.editEmail(FindPlayerByInfo.findByUserName("atarhz"),"hesamasna@yahoo.com");
+        assertEquals(FindPlayerByInfo.findByUserName("atarhz").getEmail(),"hesamasna@yahoo.com");
+    }
+    @Test
     public void testEditPhoneNumber() throws InvalidPhoneNumberException {
-        Player player = new Player("ata","rhz",1,"atarhz","11223344","ataarahimzadeh@gmail.com","09365909061");
-        Player.AddNewPlayer(player);
-        Edit.editPhoneNumber(player,"09121111111");
-        assertEquals(player.getPhoneNum(),"09121111111");
+
+        Edit.editPhoneNumber(FindPlayerByInfo.findByUserName("atarhz"),"09121111111");
+        assertEquals(FindPlayerByInfo.findByUserName("atarhz").getPhoneNum(),"09121111111");
     }
 }
