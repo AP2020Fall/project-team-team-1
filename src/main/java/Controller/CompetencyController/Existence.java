6c6240d3db04bfaa11df0aedd4568bda085ea828
hessamasna <hessamasna@yahoo.com>
2020-12-11 19:27:51 +0330

    Change GameName Completed in Backend and Frontend Amir should add it in adminMenu for changing

--- src/main/java/Controller/CompetencyController/Existence.java
+++ src/main/java/Controller/CompetencyController/Existence.java
@@ -5,45 +5,7 @@ import Model.PlatoModel.*;
 
 public class Existence {
 
-//    public static boolean checkUserNameExistence(String userName) {
-//        boolean result = false;
-//        for (User user : User.users) {
-//            if (user.getUserName().equals(userName)) {
-//                result = true;
-//                break;
-//            }
-//        }
-//        return result;
-//    }
-//
-//    public static boolean checkEmailExistence(String email) {
-//        boolean result = false;
-//        for (User user : User.users) {
-//            if (user.getEmail().equals(email)) {
-//                result = true;
-//                break;
-//            }
-//        }
-//        return result;
-//    }
-//
-//    public static boolean checkPassword(String username, String password) {
-//        boolean result = false;
-//        User userByUsername = null;
-//        for (User user : Player.users) {
-//            if (user.getUserName().equals(username)) {
-//                userByUsername = user;
-//                break;
-//            }
-//        }
-//        if (userByUsername != null) {
-//            if (userByUsername.getPassword().equals(password)) {
-//                result = true;
-//            }
-//        }
-//
-//        return result;
-//    }
+
 
     public static boolean checkUserNameExistence(String userName) {
         boolean result = false;
@@ -119,7 +81,7 @@ public class Existence {
 
     public static boolean checkGameExistence(String gameName){
         boolean result = false;
-        if (gameName.equalsIgnoreCase("DotsAndBoxes") || gameName.equalsIgnoreCase("BattleShip"))
+        if (gameName.equalsIgnoreCase(Games.getGames().get(0).getGameName()) || gameName.equalsIgnoreCase(Games.getGames().get(1).getGameName()))
                 result = true;
 
         return result;
