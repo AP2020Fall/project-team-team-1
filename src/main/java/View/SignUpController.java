5f6c458caeec887381a44eaa0526a80746e4512c
Amirzgh1 <Amirgh5665@gmail.com>
2020-12-29 14:41:36 +0330

    goh

--- src/main/java/View/SignUpController.java
+++ src/main/java/View/SignUpController.java
@@ -1,4 +1,14 @@
 package View;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class SignUpController {
+    @FXML
+    Button btnExit;
+    @FXML
+    private void appExit(ActionEvent event){
+        System.exit(1);
+    }
 }
