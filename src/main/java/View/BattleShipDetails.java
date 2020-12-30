5e50e5cfa29b29baf0abcbb14c2e19336fdb4f49
hessamasna <hessamasna@yahoo.com>
2020-12-30 22:21:32 +0330

    BattleShipDetails almost compiled

--- src/main/java/View/BattleShipDetails.java
+++ src/main/java/View/BattleShipDetails.java
@@ -2,7 +2,9 @@ package View;
 
 import Controller.AdminController.AdminGeneralController;
 import Controller.PlayerController.PlayerGeneralController;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.text.Text;
 
@@ -14,11 +16,17 @@ public class BattleShipDetails {
     Label topic;
     @FXML
     Text detail;
+    @FXML
+    Button btnExit;
 
     private void setTopic(){
         topic.setText(adminGeneralController.firstGameNameGetter()+"'s Details");
     }
     private void setDetail(){
         detail.setText(playerGeneralController.battleDetails());
+    }
+    @FXML
+    private void appExit(ActionEvent event){
+        System.exit(1);
     }
 }
