e0068c1af22b47d567ab04fea13d94f9ea6cd948
ata <atarahimzadeh.1378@gmail.com>
2021-01-03 19:21:08 +0330

    run game for dots and boxes

--- src/main/java/View/DotsAndBoxesMainMenuController.java
+++ src/main/java/View/DotsAndBoxesMainMenuController.java
@@ -159,6 +159,17 @@ public class DotsAndBoxesMainMenuController implements Initializable {
         window.setScene(message);
         window.show();
     }
+    @FXML
+    private void runGame(ActionEvent actionEvent) throws IOException {
+
+        URL url = new File("src/main/resources/FXML/BattleShipRunMenu.fxml").toURI().toURL();
+        Parent register = FXMLLoader.load(url);
+        Scene message = new Scene(register);
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        window.setScene(message);
+        window.show();
+
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
