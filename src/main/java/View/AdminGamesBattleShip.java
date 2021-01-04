dce64f46ccf05ee51a77a60a38d85f9e1eac3044
Amirzgh1 <Amirgh5665@gmail.com>
2021-01-04 21:58:18 +0330

    AdminGamesBattleShip 13%

--- src/main/java/View/AdminGamesBattleShip.java
+++ src/main/java/View/AdminGamesBattleShip.java
@@ -1,4 +1,77 @@
 package View;
 
-public class AdminGamesBattleShip {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminGamesBattleShip implements Initializable {
+    @FXML
+    public Button btnGoToAdminBattleShipGame;
+    @FXML
+    public Button btnGoToAdminDotsGame;
+    @FXML
+    public Button btnDeActive;
+    @FXML
+    public Button btnActive;
+    @FXML
+    public Label lblNumbers;
+    @FXML
+    public Label lblMVP;
+    @FXML
+    public TextArea txtDetails;
+    @FXML
+    public Button btnEdit;
+    @FXML
+    public Label lblDetails;
+
+    @FXML
+    private void goToAdminDotsGame(ActionEvent event) throws IOException {
+        URL url = new File("src/main/resources/FXML/AdminGamesDotsAndBoxes.fxml").toURI().toURL();
+        Parent register = FXMLLoader.load(url);
+        Scene message = new Scene(register);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(message);
+        window.show();
+    }
+    @FXML
+    private void goToBattleGame(ActionEvent event) throws IOException {
+        URL url = new File("src/main/resources/FXML/AdminGamesBattleShip.fxml").toURI().toURL();
+        Parent register = FXMLLoader.load(url);
+        Scene message = new Scene(register);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(message);
+        window.show();
+    }
+    @FXML
+    private void goToLastMenu(ActionEvent event) throws IOException {
+        URL url = new File("src/main/resources/FXML/AdminMenu.fxml").toURI().toURL();
+        Parent register = FXMLLoader.load(url);
+        Scene message = new Scene(register);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(message);
+        window.show();
+    }
+    @FXML
+    private void appExit(ActionEvent event){
+        System.exit(1);
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
