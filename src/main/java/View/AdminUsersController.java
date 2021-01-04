7305ca7b770744a7e8a0547d5dce5dc7b3d274dc
Amirzgh1 <Amirgh5665@gmail.com>
2021-01-04 19:34:35 +0330

    AdminUsers 40%

--- src/main/java/View/AdminUsersController.java
+++ src/main/java/View/AdminUsersController.java
@@ -1,6 +1,12 @@
 package View;
 
+import Controller.AdminController.AdminGeneralController;
 import Model.PlatoModel.Player;
+import Model.PlatoModel.PlayerLog;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,6 +19,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -21,18 +28,14 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AdminUsersController implements Initializable {
+    protected static AdminGeneralController adminGeneralController = new AdminGeneralController();
 
     @FXML
     public TableView<Player> table;
 
     @FXML
-    public TableColumn<Player, Integer> tblID;
-
-    @FXML
     public TableColumn<Player, Integer> tblUsername;
 
-    @FXML
-    public TableColumn<Player, Integer> tblPoints;
 
     @FXML
     public TextField txtSearch;
@@ -54,9 +57,11 @@ public class AdminUsersController implements Initializable {
 
     @FXML
     public Button btnExit;
+    @FXML
+    public TableColumn<Player, Integer> tblID;
 
     @FXML
-    private void back(ActionEvent event) throws IOException {
+    private void backToAdminMenu(ActionEvent event) throws IOException {
         URL url = new File("src/main/resources/FXML/AdminMenu.fxml").toURI().toURL();
         Parent register = FXMLLoader.load(url);
         Scene message = new Scene(register);
@@ -64,13 +69,46 @@ public class AdminUsersController implements Initializable {
         window.setScene(message);
         window.show();
     }
+
     @FXML
-    private void exitApp(ActionEvent event){
+    private void exitApp(ActionEvent event) {
         System.exit(1);
     }
 
+    private final ObservableList<Player> players = FXCollections.observableArrayList(
+
+    );
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        lblPlayerNum.setText(String.valueOf(Player.getPlayers().size()));
+//        lblGamesNum.setText();
+//        lblMVP.setText();
+        tblID.setCellValueFactory(new PropertyValueFactory<>("UserID"));
+        tblUsername.setCellValueFactory(new PropertyValueFactory<>("userName"));
+        table.setItems(players);
+        for (Player player : Player.getPlayers()) {
+            table.getItems().add(player);
+        }
+        //-----------filter------------------//
+        FilteredList<Player> filteredList = new FilteredList<>(players, b -> true);
 
+        txtSearch.textProperty().addListener((Observable, oldValue, newValue) -> {
+            filteredList.setPredicate(player -> {
+                if (newValue.isEmpty()) {
+                    return true;
+                }
+                String lowerCaseValue = newValue.toLowerCase();
+
+                if (player.getUserName().toLowerCase().startsWith(lowerCaseValue)) {
+                    return true;
+                } else return false;
+            });
+        });
+        SortedList<Player> sortedData = new SortedList<>(filteredList);
+
+        sortedData.comparatorProperty().bind(table.comparatorProperty());
+
+        table.setItems(filteredList);
     }
 }
