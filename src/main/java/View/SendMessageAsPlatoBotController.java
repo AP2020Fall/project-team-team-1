e631500cfe5b8d2058c27749d0d41ba3a597afc9
Amirzgh1 <Amirgh5665@gmail.com>
2021-01-08 16:59:37 +0330

    ChatBox

--- src/main/java/View/SendMessageAsPlatoBotController.java
+++ src/main/java/View/SendMessageAsPlatoBotController.java
@@ -1,4 +1,34 @@
 package View;
 
-public class SendMessageAsPlatoBotController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SendMessageAsPlatoBotController implements Initializable {
+    @FXML
+    public Button btnSend;
+    @FXML
+    public TextField txtMessage;
+    @FXML
+    public VBox vbox;
+
+    @FXML
+    public void sendMessage(ActionEvent event) {
+
+    }
+
+    private void showMessages(){
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
