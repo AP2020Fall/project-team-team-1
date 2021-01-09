ce6a3cc8ff61987949d357db3f4b3b7680f29a27
hessamasna <hessamasna@yahoo.com>
2021-01-09 12:31:04 +0330

    reformater for time

--- src/main/java/Controller/PlayerController/ViewPlatoBotMessages.java
+++ src/main/java/Controller/PlayerController/ViewPlatoBotMessages.java
@@ -2,13 +2,15 @@ package Controller.PlayerController;
 
 import Model.PlatoModel.Message;
 
+import java.time.format.DateTimeFormatter;
+
 public class ViewPlatoBotMessages {
 
     public static String viewBotMessages ()  {
         StringBuilder viewBotMessages = new StringBuilder();
-
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");
         for (Message message : Message.getMessages()) {
-            viewBotMessages.append(message.getMassageID()).append(" : ").append(message.getText()).append("    ,").append(message.getDate()).append(" , ").append(message.getTime().getHour()).append(":").append(message.getTime().getMinute()).append("$");
+            viewBotMessages.append(message.getMassageID()).append(" : ").append(message.getText()).append("    ,").append(message.getDate()).append(" , ").append(message.getTime().format(dtf)).append("$");
         }
         return String.valueOf(viewBotMessages);
 
