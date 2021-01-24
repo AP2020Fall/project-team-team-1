3682d5e78c31c451d89eea424a4fc6c0deeb0e18
hessamasna <hessamasna@yahoo.com>
2021-01-24 17:37:10 +0330

    player FriendsMainMenu done

--- src/main/java/Client/DataLoader.java
+++ src/main/java/Client/DataLoader.java
@@ -15,19 +15,22 @@ import java.util.ArrayList;
 public class DataLoader {
     //todo add coder
     //todo add request watcher for lot requests
+
     /***************************REGISTER***********************/
     public String register(String info) throws IOException {
         Client.getDataOutputStream().writeUTF("Register " + info);
         Client.getDataOutputStream().flush();
         return Client.getDataInputStream().readUTF();
     }
-    public String setUserProfile(String username,String path) throws IOException {
+
+    public String setUserProfile(String username, String path) throws IOException {
         Client.getDataOutputStream().writeUTF("Edit Profile Picture " + username + " " + path);
         Client.getDataOutputStream().flush();
         return Client.getDataInputStream().readUTF();
     }
-    public String login(String username,String password) throws IOException {
-        Client.getDataOutputStream().writeUTF("login "+username+","+password);
+
+    public String login(String username, String password) throws IOException {
+        Client.getDataOutputStream().writeUTF("login " + username + "," + password);
         Client.getDataOutputStream().flush();
         return Client.getDataInputStream().readUTF();
     }
@@ -88,13 +91,27 @@ public class DataLoader {
         Client.getDataOutputStream().flush();
         return Client.getDataInputStream().readUTF();
     }
+
     public String battleShipDetails() throws IOException {
         Client.getDataOutputStream().writeUTF("load battle details");
         Client.getDataOutputStream().flush();
         return Client.getDataInputStream().readUTF();
     }
+
     public String firstGameNameGetter() throws IOException {
         Client.getDataOutputStream().writeUTF("first game name");
+        Client.getDataOutputStream().flush();
+        return Client.getDataInputStream().readUTF();
+    }
+
+    public String playerFriends(String username) throws IOException {
+        Client.getDataOutputStream().writeUTF("Friends List " + username);
+        Client.getDataOutputStream().flush();
+        return Client.getDataInputStream().readUTF();
+    }
+
+    public String playerRequests(String username) throws IOException {
+        Client.getDataOutputStream().writeUTF("Friends Requests " + username);
         Client.getDataOutputStream().flush();
         return Client.getDataInputStream().readUTF();
     }
